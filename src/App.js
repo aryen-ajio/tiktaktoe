68e9aa9d60e58523f3a9859e0aad9714ecdf5b2b
Aryen Jim Bejagan <aryen@ajio.co.jp>
2022-09-02 17:46:10 +0800

    WIP emoji as markers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Picker from 'emoji-picker-react';
 import Board from './components/Board';
 
 const calculateWinner = (squares) => {
@@ -27,6 +28,10 @@ const calculateWinner = (squares) => {
 
 const App = () => {
 
+  const [gameStarted, setGameStarted] = useState(false);
+  const [showPicker, setShowPicker] = useState(true);
+  const [chosenEmojis, setChosenEmojis] = useState({ p1: null, p2: null });
+  console.log(chosenEmojis);
   const [history, setHistory] = useState([
     {
       squares: Array(9).fill(null),
@@ -34,6 +39,18 @@ const App = () => {
   ]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
+
+  const onEmojiClick = (event, emojiObject) => {
+    console.log(emojiObject.emoji);
+    if (!chosenEmojis.p1) {
+      setChosenEmojis({...chosenEmojis, p1: emojiObject});
+    } else {
+      if (emojiObject.emoji !== chosenEmojis.p1.emoji) {
+        setChosenEmojis({...chosenEmojis, p2: emojiObject});
+        setShowPicker(false);
+      }
+    }
+  };
 
   const resetGame = () => {
     setHistory([{squares: Array(9).fill(null)}]);
@@ -80,30 +97,64 @@ const App = () => {
 
   let status;
   if (winner) {
-    status = <p>Winner: {winner === "X" ? <span className="text-blue-500 font-bold">X</span> : <span className="text-red-500 font-bold">O</span>}</p>;
+    status = <p>Winner: {winner === "X" ? <span className="text-blue-500 font-bold">X 🏆</span> : <span className="text-red-500 font-bold">O 🏆</span>}</p>;
   } else {
     status = <p>Next player: {xIsNext ? <span className="text-blue-500 font-bold">X</span> : <span className="text-red-500 font-bold">O</span>}</p>;
   }
 
   return (
-    <div className="game">
-      <div className="flex">
-        <div className="game-board">
-          <Board
-            squares={current}
-            onClick={(i) => handleClick(i)}
-          />
+    <div className="game font-poppins">
+      {gameStarted ? (
+        <div className="flex">
+          <div className="game-board">
+            <Board
+              squares={current}
+              onClick={(i) => handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div className="flex items-start justify-between">
+              <h1 className="text-4xl font-bold mr-5">Tiktaktoe</h1>
+              <button
+                className="text-lg text-white font-bold bg-red-500 px-[10px] py-[3px] border border-red-600 rounded mb-2"
+                onClick={resetGame}>
+                  ↻
+              </button>
+            </div>
+            <div className="text-lg mb-2">{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
-        <div className="game-info">
+      ) : (
+        <div className="flex flex-col items-start justify-center">
+          <div className="flex flex-col items-start justify-between">
+            <p>Player 1 emoji: {chosenEmojis.p1 ? `${chosenEmojis.p1.emoji}` : 'No emoji chosen'}</p>
+            <p>Player 2 emoji: {chosenEmojis.p2 ? `${chosenEmojis.p2.emoji}` : 'No emoji chosen'}</p>
+          </div>
+          {showPicker ?
+            <Picker
+              onEmojiClick={onEmojiClick}
+              groupVisibility={{
+                recently_used: false,
+                smileys_people: false,
+                food_drink: false,
+                travel_places: false,
+                activities: false,
+                objects: false,
+                symbols: false,
+                flags: false,
+              }}
+            /> :
+            null
+          }
           <button
-            className="text-white bg-red-500 p-2 border border-red-600 rounded mb-2"
-            onClick={resetGame}>
-              Reset game
+            className="text-xl text-white font-bold bg-green-600 px-8 py-4 mt-4 border border-green-700 rounded-xl"
+            onClick={() => setGameStarted(true)}>
+              Start game
           </button>
-          <div className="mb-2">{status}</div>
-          <ol>{moves}</ol>
         </div>
-      </div>
+      )
+      }
     </div>
   );
 }
